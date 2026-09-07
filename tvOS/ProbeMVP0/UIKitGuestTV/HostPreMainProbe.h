6da6a89a1e6c960273315a87741d19782e01752c
andremlk <84326974+andremlk@users.noreply.github.com>
2026-09-06 18:52:27 -0600

    feat(tvOS): add guest observer for host pre-main bootstrap

---
+++ tvOS/ProbeMVP0/UIKitGuestTV/HostPreMainProbe.h
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+
+BOOL LCTVHostPreMainProbeEnabled(void);
+BOOL LCTVHostPreMainProbePassed(void);
+NSString *LCTVHostPreMainProbeTitle(void);
+NSString *LCTVHostPreMainProbeStatus(void);
+NSString *LCTVHostPreMainProbeExpectation(void);
+NSString *LCTVHostPreMainProbeSnapshot(void);
